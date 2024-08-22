6e89206fc5399f899b1e3d82bc6e08d459f503d6
Agustin-del <eddyruveda@gmail.com>
2024-08-21 23:11:19 -0300

    dia5

--- dia5/main.js
+++ dia5/main.js
@@ -3,28 +3,31 @@ let frutas = [];
 let lacteos = [];
 let congelados = [];
 let dulces = []
-let pregunta = "";
-do {
-    pregunta = prompt("¿Deseas agregar un alimento a tu lista de compras? si o no");
-     if (pregunta == "si") {
-        let alimento = prompt("¿Cuál es el alimento que deseas agregar? ");
-        let categoria = prompt("¿En qué categoría se encaja el alimento? frutas, lacteos,  congelados, dulces");
-        switch (categoria) {
-            case "frutas":
-                frutas.push(alimento);
-                break;
-            case "lacteos":
-                lacteos.push(alimento);
-                break;
-            case "congelados":
-                congelados.push(alimento);
-                break;
-            case "dulces":
-                dulces.push(alimento);
-                break;
-        }
+
+while(true) {
+    let pregunta = prompt("¿Deseas agregar un alimento a tu lista de compras? si o no");
+
+    if(pregunta === "no") {
+        break;
     }
-} while (pregunta == "si");
+    let alimento = prompt("¿Cuál es el alimento que deseas agregar? ");
+    let categoria = prompt("¿En qué categoría se encaja el alimento? frutas, lacteos,  congelados, dulces");
+    switch (categoria) {
+        case "frutas":
+            frutas.push(alimento);
+            break;
+        case "lacteos":
+            lacteos.push(alimento);
+            break;
+        case "congelados":
+            congelados.push(alimento);
+            break;
+        case "dulces":
+            dulces.push(alimento);
+            break;
+    }
+}
+
 
 if (frutas.length > 0) {
     console.log(`Frutas: ${frutas.join()}`);
